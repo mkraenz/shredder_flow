3f51f3d392f27597edabc6662949eb0a6b2845b0
duboka <duboka909@googlemail.com>
2015-06-07 19:19:31 +0200

    #7 triangulate added

--- src/shredder_flow/PolygonalLine.java
+++ src/shredder_flow/PolygonalLine.java
@@ -2,12 +2,14 @@ package shredder_flow;
 
 import de.jtem.discreteCurves.tutorial.DraggablePolygonPlugin;
 import de.jtem.java2d.SceneComponent;
+import de.jtem.java2dx.Point2DList;
 import de.jtem.java2dx.modelling.DraggablePoint2DList;
 import de.jtem.java2dx.modelling.DraggablePolygon2D;
 import de.jtem.java2dx.plugin.Java2DView;
 import de.jtem.java2dx.plugin.Java2DViewer;
 import de.jtem.jrworkspace.plugin.Controller;
 import de.jtem.jrworkspace.plugin.Plugin;
+import de.jtem.numericalMethods.geometry.meshGeneration.ruppert.Ruppert;
 
 public class PolygonalLine extends Plugin {
 
@@ -23,8 +25,28 @@ public class PolygonalLine extends Plugin {
 		root.addChild(polygon.getViewScene());
 		// add the control scene
 		root.addChild(polygon.getControlScene());
-		
+
 		super.install(c);
+	}
+
+	public double[] triangulate(DraggablePolygon2D polygon2d) {
+
+		DraggablePoint2DList listOfContolPoints = polygon2d.getControlPoints();
+		Point2DList pointList = listOfContolPoints.getModel();
+		double[][] boundaryPoints = pointList.toDoubleArray();
+		Ruppert ruppert = new Ruppert(boundaryPoints);
+		ruppert.getIndices();
+		double[] points = ruppert.getPoints();
+		return points;
+	}
+
+	public double[][] rearrange(double[] points) {
+		double[][] newPoints = new double[points.length / 2][2];
+		for (int i = 0; i < points.length; i = i + 2) {
+			newPoints[i][0] = points[i];
+			newPoints[i][1] = points[i + 1];
+		}
+		return newPoints;
 	}
 
 	public static void main(String[] args) {
@@ -32,5 +54,5 @@ public class PolygonalLine extends Plugin {
 		vr.registerPlugin(new PolygonalLine());
 		vr.startup();
 	}
-	
+
 }
