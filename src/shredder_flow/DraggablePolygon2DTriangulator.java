b38f8d8b7743a63baba2e953f822bf5bbd713468
Mirco Kraenz <prosingularity@yahoo.de>
2015-06-15 20:14:31 +0200

    #7 triangulate propertly and visualize vertices

--- src/shredder_flow/DraggablePolygon2DTriangulator.java
+++ src/shredder_flow/DraggablePolygon2DTriangulator.java
@@ -6,6 +6,7 @@ import de.jtem.java2dx.modelling.DraggablePolygon2D;
 import de.jtem.numericalMethods.geometry.meshGeneration.ruppert.Ruppert;
 
 public class DraggablePolygon2DTriangulator {
+	private static final int ANGLE_CONSTRAINT_IN_DEGREE = 10;
 	private double[][] points;
 	private int[] indices;
 
@@ -19,20 +20,34 @@ public class DraggablePolygon2DTriangulator {
 
 	public void triangulate(DraggablePolygon2D polygon2d) {
 		DraggablePoint2DList listOfControlPoints = polygon2d.getControlPoints();
-		Point2DList pointList = listOfControlPoints.getModel();
+		Point2DList controlPointList = listOfControlPoints.getModel();
+		double[][] pointsForRuppert = rearrangeAsAnArrayForRuppert(controlPointList);
+		
+		Ruppert ruppert = triangulateWithRuppert(pointsForRuppert);
+		
+		this.indices = ruppert.getIndices();
+		double[] triangulationVertices = ruppert.getPoints();
+		this.points = rearrangeAsTwoDimensionalArray(triangulationVertices);
+	}
+
+	private Ruppert triangulateWithRuppert(double[][] pointsForRuppert) {
+		Ruppert ruppert = new Ruppert(pointsForRuppert);
+		ruppert.setAngleConstraint(ANGLE_CONSTRAINT_IN_DEGREE);
+		ruppert.refine();
+		return ruppert;
+	}
+
+	private double[][] rearrangeAsAnArrayForRuppert(Point2DList pointList) {
 		double[][] boundaryPoints = pointList.toDoubleArray();
 		double[][] pointsForRuppert = new double[1][boundaryPoints.length * 2];
 		for (int i = 0; i < boundaryPoints.length; i++) {
 			pointsForRuppert[0][i * 2] = boundaryPoints[i][0];
 			pointsForRuppert[0][i * 2 + 1] = boundaryPoints[i][1];
 		}
-		Ruppert ruppert = new Ruppert(pointsForRuppert);
-		indices = ruppert.getIndices();
-		double[] ruppertPoints = ruppert.getPoints();
-		points = rearrange(ruppertPoints);
+		return pointsForRuppert;
 	}
 
-	private double[][] rearrange(double[] points) {
+	private double[][] rearrangeAsTwoDimensionalArray(double[] points) {
 		double[][] newPoints = new double[points.length / 2][2];
 		for (int i = 0; i < points.length; i = i + 2) {
 			newPoints[i / 2][0] = points[i];
