a86e027eb17e751b234e5846177eab90db874a08
Mirco Kraenz <prosingularity@yahoo.de>
2015-06-16 01:11:58 +0200

    #14: TEST DraggablePolygon2DTriangulator
    Write Tests for TriangulationDomain

---
+++ test/shredder_flow/DraggablePolygon2DTriangulatorTest.java
@@ -0,0 +1,63 @@
+package shredder_flow;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.jtem.java2dx.modelling.DraggablePoint2DList;
+import de.jtem.java2dx.modelling.DraggablePolygon2D;
+
+public class DraggablePolygon2DTriangulatorTest {
+
+	private DraggablePolygon2D boundaryPolygon;
+	private DraggablePolygon2DTriangulator triangulator;
+
+	@Before
+	public void setUp() throws Exception {
+		int BOUNDARY_CURVE_POINT_COUNT = 100;
+		boundaryPolygon = new DraggablePolygon2D(BOUNDARY_CURVE_POINT_COUNT,
+				DraggablePoint2DList.CIRCULAR, 0, 0, 1, true, true);
+		triangulator = new DraggablePolygon2DTriangulator();
+		triangulator.triangulate(boundaryPolygon);
+	}
+
+	@Test
+	public void testThatNeverFails() throws Exception {
+		assertTrue(true);
+		;
+	}
+
+	@Test
+	public void testTriangulateIndicesAndPointsNotNull() throws Exception {
+		assertNotNull(triangulator.getIndices());
+		assertNotNull(triangulator.getPointsAs2DimensionalArray());
+		assertNotNull(triangulator.getPointsAsOneDimensionalArray());
+	}
+
+	@Test
+	public void testNonemptyIndicesAndPoints() throws Exception {
+		assertNotEquals(0, triangulator.getIndices());
+		assertNotEquals(0, triangulator.getPointsAs2DimensionalArray());
+		assertNotEquals(0, triangulator.getPointsAsOneDimensionalArray());
+	}
+
+	@Test
+	public void testEachPointIsVertexOfSomeTriangle() throws Exception {
+		int[] indices = triangulator.getIndices();
+		int[] vertexIndices = new int[boundaryPolygon.getDefaultPointCount()];
+		for (int i = 0; i < indices.length; i++) {
+			setOneIfVertexIsInTriangle(indices, vertexIndices, i);
+		}
+		for (int i = 0; i < vertexIndices.length; i++){
+			assertNotNull(vertexIndices[i]);
+		}
+	}
+
+	private void setOneIfVertexIsInTriangle(int[] indices, int[] vertexIndices, int i) {
+		if (indices[i] < vertexIndices.length) {
+			vertexIndices[indices[i]] = 1;
+		}
+	}
+
+}
