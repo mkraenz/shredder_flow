9970717c02bf1c3b163426bfcab8288b169215f9
Mirco Kraenz <prosingularity@yahoo.de>
2015-06-26 14:44:46 +0200

    #18: RENAME MovementStrategy
    create a triangulation and visualize it

---
+++ src/shredder_flow/logic/MovementStrategy.java
@@ -0,0 +1,9 @@
+package shredder_flow.logic;
+
+public class MovementStrategy {
+
+	public void setNextPositionAndTriangle(double deltaT, Particle particle) {
+		// TODO Auto-generated method stub
+	}
+
+}
