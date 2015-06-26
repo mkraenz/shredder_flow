6c8bd7018353058815cf95fca997ad4319ba5e02
Mirco Kraenz <prosingularity@yahoo.de>
2015-06-26 13:31:08 +0200

    #18: RENAME RunMe
    create a triangulation and visualize it

--- src/shredder_flow/RunMe.java
+++ src/shredder_flow/RunMe.java
@@ -1,10 +1,8 @@
 package shredder_flow;
 
-import shredder_flow.triangulated_domain.Visualizer;
-
 public class RunMe {
 	public static void main(String[] args) {
-		Visualizer.main(null);
+		Builder.main(args);
 	}
 	
 	
