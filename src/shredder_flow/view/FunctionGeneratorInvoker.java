9ab7fbfaddc69c262e6bd8d3447b4b00cc449fbc
Mirco Kraenz <prosingularity@yahoo.de>
2015-06-29 15:50:56 +0200

    #19: ADD generate random function values
    randomly assign function values to vertices

--- src/shredder_flow/view/FunctionGeneratorInvoker.java
+++ src/shredder_flow/view/FunctionGeneratorInvoker.java
@@ -1,5 +1,11 @@
 package shredder_flow.view;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+
 import de.jtem.java2dx.plugin.View2DShrinkPanelPlugin;
 import shredder_flow.logic.FunctionGenerator;
 
@@ -9,6 +15,7 @@ public class FunctionGeneratorInvoker extends View2DShrinkPanelPlugin {
 
 	public FunctionGeneratorInvoker(FunctionGenerator generator) {
 		this.generator = generator;
+		addGuiElements();
 	}
 
 	public void invokeGeneration() {
@@ -19,5 +26,34 @@ public class FunctionGeneratorInvoker extends View2DShrinkPanelPlugin {
 		 * it in a more suitable way. See TriangulationInvoker.addGuiElements()
 		 * for reference.
 		 */
+	}
+
+	private void addGuiElements() {
+		final int ROWS = 3;
+		final int COLUMNS = 2;
+		shrinkPanel.setLayout(new GridLayout(ROWS, COLUMNS));
+		addFunctionSelectBox();
+		addButton(new ApplyFunctionAction(), "Apply");
+	}
+
+	private void addFunctionSelectBox() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void addButton(AbstractAction action, String caption) {
+		final JButton button = new JButton();
+		button.setAction(action);
+		shrinkPanel.add(button);
+		button.setText(caption);
+	}
+
+	class ApplyFunctionAction extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO
+		}
 	}
 }
