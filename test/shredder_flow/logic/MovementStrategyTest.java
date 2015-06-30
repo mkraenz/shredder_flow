8e2b89f852dc447a6c8f5a68e7f8bcf9bf1fb7f1
Mirco Kraenz <prosingularity@yahoo.de>
2015-06-30 22:15:45 +0200

    #17: CLEANUP organize imports
    implement MovementStrategy

--- test/shredder_flow/logic/MovementStrategyTest.java
+++ test/shredder_flow/logic/MovementStrategyTest.java
@@ -2,8 +2,6 @@ package shredder_flow.logic;
 
 import static org.junit.Assert.*;
 
-import java.io.ObjectInputStream.GetField;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,7 +19,7 @@ public class MovementStrategyTest {
 		vertex2 = new Vertex(0, 1);
 		vertex3 = new Vertex(1, 0);
 	}
-	
+
 	@Test
 	public void testParticleUpdateOneStepInSingleTriangle() throws Exception {
 		TriangulationVertexList verticesForBigTriangle = new TriangulationVertexList();
@@ -36,47 +34,48 @@ public class MovementStrategyTest {
 				particleListForBigTriangle, bigTriangleList);
 		creatorForBigTriangle.addParticle(0, 0);
 		Particle particle = particleListForBigTriangle.get(0);
-		
-		bigTriangle.getFieldVector().set(800.324,123.42);
+
+		bigTriangle.getFieldVector().set(800.324, 123.42);
 		particle.update(1);
-		assertEquals(800.324, particle.getX(),0);
-		assertEquals(123.42, particle.getY(),0);
+		assertEquals(800.324, particle.getX(), 0);
+		assertEquals(123.42, particle.getY(), 0);
 	}
-	
+
 	@Test
-	public void testParticleMoveOverTwoTrianglesWithoutTouchingEdge() throws Exception {
+	public void testParticleMoveOverTwoTrianglesWithoutTouchingEdge()
+			throws Exception {
 		Triangle leftTriangle = createTriangle(vertex0, vertex1, vertex2);
 		Triangle rightTriangle = createTriangle(vertex1, vertex2, vertex3);
-		
+
 		TriangleList triangles = new TriangleList();
 		triangles.add(leftTriangle);
 		triangles.add(rightTriangle);
-		
+
 		leftTriangle.getFieldVector().set(1, 0);
-		
-		Particle particle = new Particle(-1.0/2, 0, leftTriangle, new MovementStrategy(triangles));
-		
-		particle.update(1.0/4);
-		assertEquals(-1.0/4, particle.getX(), 0);
+
+		Particle particle = new Particle(-1.0 / 2, 0, leftTriangle,
+				new MovementStrategy(triangles));
+
+		particle.update(1.0 / 4);
+		assertEquals(-1.0 / 4, particle.getX(), 0);
 		assertEquals(0, particle.getY(), 0);
 		assertEquals(leftTriangle, particle.getTriangle());
-		
+
 		particle.update(0);
-		assertEquals(-1.0/4, particle.getX(), 0);
+		assertEquals(-1.0 / 4, particle.getX(), 0);
 		assertEquals(0, particle.getY(), 0);
 		assertEquals(leftTriangle, particle.getTriangle());
-		
-		particle.update(1.0/4);
+
+		particle.update(1.0 / 4);
 		assertEquals(0, particle.getX(), 0);
 		assertEquals(0, particle.getY(), 0);
 		assertEquals(leftTriangle, particle.getTriangle());
-		
-		particle.update(1.0/4);
-		assertEquals(1.0/4, particle.getX(), 0);
+
+		particle.update(1.0 / 4);
+		assertEquals(1.0 / 4, particle.getX(), 0);
 		assertEquals(0, particle.getY(), 0);
 		assertEquals(rightTriangle, particle.getTriangle());
-		
-		
+
 	}
 
 	private Triangle createTriangle(Vertex vertex0, Vertex vertex1,
