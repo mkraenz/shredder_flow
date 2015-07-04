314e30074fa18b0dceb115ffda1aa42346002713
Mirco Kraenz <prosingularity@yahoo.de>
2015-07-04 18:14:28 +0200

    #51: tests run, but symplectic field is strange maybe
    Gradient vectorfield from function

--- src/shredder_flow/logic/VectorFieldGenerator.java
+++ src/shredder_flow/logic/VectorFieldGenerator.java
@@ -58,13 +58,20 @@ public class VectorFieldGenerator {
 	 */
 	private Vector2d getGradient(Triangle triangle) {
 		Point2d v1 = triangle.getVertices().get(0).getPosition();
+		double x1 = v1.x;
+		double y1 = v1.y;
 		Point2d v2 = triangle.getVertices().get(1).getPosition();
+		double x2 = v2.x;
+		double y2 = v2.y;
 		Point2d v3 = triangle.getVertices().get(2).getPosition();
+		double x3 = v3.x;
+		double y3 = v3.y;
 		double f1 = triangle.getVertices().get(0).getFunctionValue();
 		double f2 = triangle.getVertices().get(1).getFunctionValue();
 		double f3 = triangle.getVertices().get(2).getFunctionValue();
-		double gradX = 0; // TODO write the correct formula here
-		double gradY = 0;// TODO write the correct formula here
+		double gradY = (f1 -f3)*(x3-x2);
+		gradY = gradY / ((y1-y3)*(x3-x2) - (x1-x3)*(y3-y2));
+		double gradX = (f3-f2 - gradY*(y3-y2))/(x3-x2);
 		return new Vector2d(gradX, gradY);
 	}
 }
