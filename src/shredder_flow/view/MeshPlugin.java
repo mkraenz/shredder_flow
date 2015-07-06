38fcbcd5fff8b42aa9689941276735526bc774a8
Tobi <horst@tu>
2015-07-06 12:37:09 +0200

    #27 draw FieldVector with testvector in direction (1,1) through center of triangle should be updated with creation of vectorfield

--- src/shredder_flow/view/MeshPlugin.java
+++ src/shredder_flow/view/MeshPlugin.java
@@ -35,8 +35,7 @@ public class MeshPlugin extends Plugin {
 
 		drawVertices(verticesSceneComponent, model.getVertices());
 		drawEdges(edgesSceneComponent, model.getTriangles());
-		// TODO: draw field vectors
-		// drawFieldVectors(vectorsSceneComponent, model.getTriangles());
+		drawFieldVectors(vectorsSceneComponent, model.getTriangles());
 
 		verticesSceneComponent.fireAppearanceChange();
 	}
@@ -92,17 +91,76 @@ public class MeshPlugin extends Plugin {
 			TriangleList triangles) {
 		for (Triangle triangle : triangles) {
 			// TODO: if(triangle.getFieldVector().length() != 0)
-			SceneComponent fieldVectorSceneComponent = getFieldVectorSceneComponent(triangle
-					.getFieldVector());
+			SceneComponent fieldVectorSceneComponent = getFieldVectorSceneComponent(triangle);
 			sceneComponent.addChild(fieldVectorSceneComponent);
 		}
 	}
 
-	private SceneComponent getFieldVectorSceneComponent(FieldVector fieldVector) {
+	private SceneComponent getFieldVectorSceneComponent(Triangle triangle) {
 		// TODO Auto-generated method stub
-		return null;
+		SceneComponent arrowSceneComponent = new SceneComponent();
+		double length = getTriangleLength(triangle);
+		//double[] normedFieldVec = getNormedFieldVector(triangle.getFieldVector(),length/8);
+		double[] normedFieldVec ={length/4,length/4}; // for test generation
+		double angle = Math.PI/4 -Math.atan2(normedFieldVec[1], normedFieldVec[0]);
+		double s = Math.sin(angle)*length/12;
+		double c = Math.cos(angle)*length/12;
+		Vertex center = getTriangleCenter(triangle);
+		Vertex root = getRootVertex(center, normedFieldVec);
+		Vertex peak = getPeakVertex(center, normedFieldVec);
+		Vertex right = new Vertex(peak.getX()-s, peak.getY()-c);
+		Vertex left = new Vertex(peak.getX()-c, peak.getY()+s);
+		arrowSceneComponent.addChild(getEdgeSceneComponent(root, peak));
+		arrowSceneComponent.addChild(getEdgeSceneComponent(peak, left));
+		arrowSceneComponent.addChild(getEdgeSceneComponent(peak, right));
+		return arrowSceneComponent;
+	}
+	
+	//returns length of smallest side of triangle
+	private double getTriangleLength(Triangle triangle){
+		Vertex vertex0 = triangle.getVertices().get(0);
+		double x0 = vertex0.getX();
+		double y0 = vertex0.getY();
+		Vertex vertex1 = triangle.getVertices().get(1);
+		double x1 = vertex1.getX();
+		double y1 = vertex1.getY();
+		Vertex vertex2 = triangle.getVertices().get(2);
+		double x2 = vertex2.getX();
+		double y2 = vertex2.getY();
+		
+		double a = Math.sqrt((x0-x1)*(x0-x1)+(y0-y1)*(y0-y1));
+		double b = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+		double c = Math.sqrt((x2-x0)*(x2-x0)+(y0-y2)*(y0-y2));
+		return Math.min(Math.min(a, b),c);
 	}
 
+	private Vertex getRootVertex(Vertex center, double[] normedFieldVec){
+			Vertex vertex = new Vertex(center.getX() - normedFieldVec[0], center.getY() - normedFieldVec[1]);
+			return vertex;
+	}
+
+	private Vertex getPeakVertex(Vertex center, double[] normedFieldVec){
+			Vertex vertex = new Vertex(center.getX() + normedFieldVec[0], center.getY() + normedFieldVec[1]);
+			return vertex;
+	}
+	//return array of normed FieldVector with length = l
+	private double[] getNormedFieldVector(FieldVector fieldVec, double l){
+		double[] normedVector = new double[2];
+		double length = Math.sqrt(fieldVec.x*fieldVec.x+fieldVec.y*fieldVec.y);
+		normedVector[0]=(fieldVec.x/length)*l;
+		normedVector[1]=(fieldVec.y/length)*l;
+		return normedVector;
+		
+	}
+	
+	//return vertex of center
+	private Vertex getTriangleCenter(Triangle triangle){
+		Vertex vertex0 = triangle.getVertices().get(0);
+		Vertex vertex1 = triangle.getVertices().get(1);
+		Vertex vertex2 = triangle.getVertices().get(2);
+		Vertex center = new Vertex((vertex0.getX()+vertex1.getX()+vertex2.getX())/3,(vertex0.getY()+vertex1.getY()+vertex2.getY())/3);
+		return center;
+	}
 	@Override
 	public void install(Controller c) throws Exception {
 		super.install(c);
